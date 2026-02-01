eb9432b48892bd8100961bb60cd2da5b3ac0715c
mauroxavierjr <mauroxavier.net@gmail.com>
2026-02-01 10:42:36 -0300

    update files

---
+++ 10-prometheus/eksctl-install-cluster.sh
@@ -0,0 +1,1 @@
+eksctl create cluster --name=eks-cluster --version=1.34 --region=us-east-1 --nodegroup-name=eks-cluster-nodegroup --node-type=t3.medium --nodes=2 --nodes-min=1 --nodes-max=2 --managed
